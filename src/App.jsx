19ebd876f612abe3c5a253fa0daaf311ce965064
Constanza Delfino <111506427+cdelfino@users.noreply.github.com>
2023-12-15 02:55:22 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Test from "./Test";
 import Header from "./components/layout/Header";
 
 function App() {
   return (
     <div>
       <Header />
+      <Test />
     </div>
   );
 }
---
+++ src/Test.jsx
@@ -0,0 +1,6 @@
+import styles from "./Test.module.css";
+const Test = () => {
+  return <h1 className={styles.title}>TEST</h1>;
+};
+
+export default Test;
